20ea3a1db9363b3b4b933c1c76296b4120b36911
a171417 <1714178382@qq.com>
2019-01-19 14:42:43 +0800

    11

---
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolInitializer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        protected override void Seed(SchoolContext context)
+        {
+            var students = new List<Student>
+           {
+               new Student {Name="段誉",EnrollmentDate=DateTime.Parse("2019-1-1") },
+               new Student {Name="乔峰",EnrollmentDate=DateTime.Parse("2019-1-1") },
+               new Student {Name="慕容复",EnrollmentDate=DateTime.Parse("2019-1-1") },
+               new Student {Name="虚竹",EnrollmentDate=DateTime.Parse("2019-1-1") },
+               new Student {Name="小黑",EnrollmentDate=DateTime.Parse("2019-1-1") },
+               new Student {Name="小强",EnrollmentDate=DateTime.Parse("2019-1-1") },
+               new Student {Name="小伟",EnrollmentDate=DateTime.Parse("2019-1-1") },
+               new Student {Name="小屁",EnrollmentDate=DateTime.Parse("2019-1-1") }
+
+           };
+            //将课程数据加入实体集
+            students.ForEach(st => context.Students.Add(st));
+            context.SaveChanges();
+
+            var courses = new List<Course>
+            {
+            new Course{CourseID=1050,Title="Chemistry",Credits=3,},
+            new Course{CourseID=4022,Title="Microeconomics",Credits=3,},
+            new Course{CourseID=4041,Title="Macroeconomics",Credits=3,},
+            new Course{CourseID=1045,Title="Calculus",Credits=4,},
+            new Course{CourseID=3141,Title="Trigonometry",Credits=4,},
+            new Course{CourseID=2021,Title="Composition",Credits=3,},
+            new Course{CourseID=2042,Title="Literature",Credits=4,}
+            };
+            courses.ForEach(s => context.Courses.Add(s));
+            context.SaveChanges();
+
+            var enrollments = new List<Enrollment>
+            {
+            new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
+            new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
+            new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
+            new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
+            new Enrollment{StudentID=3,CourseID=1050},
+            new Enrollment{StudentID=4,CourseID=1050,},
+            new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
+            new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
+            new Enrollment{StudentID=6,CourseID=1045},
+            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            };
+            enrollments.ForEach(s => context.Enrollments.Add(s));
+            context.SaveChanges();
+
+        }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Display (Name="姓名")]
+        public string Name { get; set; }
+        [Display(Name = "注册时间")]      
+        public DateTime EnrollmentDate { get; set; }
+
+        // public string Secret { get; set; }
+        [Display(Name = "头像")]
+        public string Image { get; set; }
+
+        [Display(Name = "选课信息")]
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
